0c617ac6f5404eb6102beaf070d3bea167d7fe65
Josh Bennett <josh.bennett77@yahoo.com>
2022-03-28 10:40:11 -0600

    Create 5 round function and display results
    I added every possible outcome to the single round function. I then
    added a function that would iterate the single round function five
    times and return the result of each round. Also added a function that
    displays the winner at the end of the 5 rounds based on who's score is
    higher than the other.

--- javascript.js
+++ javascript.js
@@ -1,25 +1,78 @@
-// random choice function
+// Computer random choice function
 function computerPlay () {
-    const compChoices = ['rock', 'paper', 'scissors'];
-    let randCompChoice = compChoices[Math.floor(Math.random() * compChoices.length)];
-    randCompChoice = randCompChoice.charAt(0).toUpperCase() + randCompChoice.slice(1);
-    return randCompChoice;
+    const choices = ['rock', 'paper', 'scissors'];
+    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    return computerSelection;
 }
+
+// Player choice function
+function playerPlay() {
+    let playerSelection = prompt('Rock, Paper, or Scissors?','');
+    return playerSelection.toLowerCase();
+}
+
+let playerScore = 0;
+let computerScore = 0;
 
 //single round function
-function playRound (playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock' && computerSelection == 'rock') {
+            return 'It\'s a tie.';
+        }   else if (playerSelection == 'rock' && computerSelection == 'paper') {
+                return 'You lose!';
+        }   else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+                return 'You win!';
+        }   else if (playerSelection == 'paper' && computerSelection == 'rock') {
+                return 'You win!';
+        }   else if (playerSelection == 'paper' && computerSelection == 'paper') {
+                return 'It\'s a tie.';
+        }   else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+                return 'You lose!';
+        }   else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+                return 'You lose!';
+        }   else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+                return 'You win!';
+        }   else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+                return 'It\'s a tie!';
+        }   else {
+                return 'Please enter a valid choice. Rock, paper, or scissors.';
+        }
+    } // playRound()
 
-    if (playerSelection == 'rock' && computerSelection == 'Paper') {
-        return 'Oh no, you lost! Paper covers rock!';
-    }   else if (playerSelection == 'rock' && computerSelection == 'Rock') {
-        return 'It\'s a tie. Go again!';
-    }   else if (playerSelection == 'rock' && computerSelection == 'Scissors') { 
-        return 'You win! Rock crushes scissors!';
-    }   else {
-        return 'Enter a choice!';
-    }
+// 5 round game function
+function game() {
+    for (let i = 0; i < 5; i++) {
+        computerSelection = computerPlay();
+        playerSelection = playerPlay();
+            let result = playRound(playerSelection, computerSelection);
+            if (result == 'You win!') {
+                playerScore++;
+                console.log('Player: ' + playerScore)
+                console.log('Computer: ' + computerScore)
+                console.log('Computer chose: ' + computerSelection)
+                console.log('You win this round!')
+            }   else if (result == 'You lose!') {
+                    computerScore++;
+                    console.log('Player: ' + playerScore)
+                    console.log('Computer: ' + computerScore)
+                    console.log('Computer chose: ' + computerSelection)
+                    console.log('You lose this round!')
+            }   else {
+                    console.log('This round ended in a tie! No points awarded.')
+            }
+        } // for loop
+    } // game()
+
+// Winner announcement function
+function displayWinner() {
+    if (playerScore > computerScore) {
+        return 'You beat the computer!';
+    }   else if (computerScore > playerScore) 
+          return 'You lost to the computer!'
 }
 
-const playerSelection = prompt('What will you choose?','').toLowerCase();
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
+console.log('Final Score!')
+console.log('Player: ' + playerScore);
+console.log('Computer: ' + computerScore);
+console.log(displayWinner());
