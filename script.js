c0db4f1d5ef9687beec7afdb63cf0482d8a09662
Josh Bennett <josh.bennett77@yahoo.com>
2022-04-12 21:26:16 -0600

    Build script to walk user through start prompts
    Deleted html body as I will be building the UI using DOM manipulation,
    including the start screen. Built the starting pop-up, asking the user
    if they would like to move forward with a game. If no, it redirects to
    my RPS github repo. If yes, the starting pop-up will be removed and the
    rules pop-up will populate, prompting the user to start the game.

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+function createDivElement (className, text, id = '') {
+	const div = document.createElement('div');
+	div.appendChild(document.createTextNode(text));
+	div.className = className;
+	div.id = id;
+	return div;
+}
+
+function referToGithub() {
+	window.open('https://github.com/JoshBennett793/RPS')
+}
+
+function wantToPlay () {
+
+	const containerWantToPlay = createDivElement('start-popup', '')
+	containerWantToPlay.appendChild(createDivElement('', 'Want to play Rock, Paper, Scissors?'));
+
+	const containerButtons = createDivElement('container-buttons', '');
+	containerButtons.appendChild(createDivElement('button btn-green', 'Yes'));
+	containerButtons.appendChild(createDivElement('button btn-red', 'No'));
+	containerWantToPlay.appendChild(containerButtons);
+
+	document.body.appendChild(containerWantToPlay);
+
+	const btnGreen = document.querySelector('.btn-green');
+	btnGreen.addEventListener('click', () => {
+		document.body.removeChild(containerWantToPlay);
+		return showRules();
+	});
+	const btnRed = document.querySelector('.btn-red');
+	btnRed.addEventListener('click', () => {
+		return referToGithub();
+	});
+	
+}
+
+function showRules() {
+
+	const containerShowRules = createDivElement('rules-popup', '');
+	containerShowRules.appendChild(createDivElement('', 'Select Rock, Paper, or Scissors. Best 2 out of 3 rounds wins. '));
+
+	const buttonContainer = createDivElement('container-buttons', '');
+	buttonContainer.appendChild(createDivElement('button btn-green', 'Play'));
+	containerShowRules.appendChild(buttonContainer);
+
+	document.body.appendChild(containerShowRules);
+
+	const btnPlay = document.querySelector('.btn-green');
+	btnPlay.addEventListener('click', () => {
+		document.body.removeChild(containerShowRules);
+	});
+}
+
 // Computer random choice function
 function computerPlay () {
   const choices = ['rock', 'paper', 'scissors'];
@@ -10,38 +63,34 @@ let computerScore = 0;
 
 //single round function
 function playRound(playerSelection, computerSelection) {
-  if ((playerSelection !== 'rock') && 
-				(playerSelection !== 'paper') && 
-					(playerSelection !== 'scissors')) {
-  	return 'invalid';
-} else if (playerSelection ===  computerSelection) {
-    return 'tie';
+	computerSelection = computerPlay();
+  if (playerSelection ===  computerSelection) {
+    console.log('tie');
 }	else if (playerSelection === 'rock' && 
 						computerSelection === 'paper') {
-    return 'lose';
+    console.log('lose');
 } else if (playerSelection === 'rock' && 
 						computerSelection === 'scissors') {
-    return 'win';
+    console.log('win');
 } else if (playerSelection === 'paper' && 
 						computerSelection === 'rock') {
-    return 'win';
+    console.log('win');
 } else if (playerSelection === 'paper' && 
 						computerSelection === 'scissors') {
-    return 'lose';
+    console.log('lose');
 } else if (playerSelection === 'scissors' && 
 						computerSelection === 'rock') {
-    return 'lose';
+    console.log('lose');
 } else if (playerSelection === 'scissors' && 
 						computerSelection === 'paper') {
-  	return 'win';
+  	console.log('win');
 }
 } // playRound()
 
 // 5 round game function
-function game() {
+/*function game() {
 //for (let i = 0; i < 5; i++) {
   	computerSelection = computerPlay();
-  	playerSelection = playerPlay();
 	  let result = playRound(playerSelection, computerSelection);
   	  if (result == 'win') {
 	  	  playerScore++;
@@ -62,7 +111,7 @@ function game() {
 					i--;
 	}
 } // for loop
-//} // game()
+//} // game()*/
 
 // Winner announcement function
 function displayWinner() {
@@ -75,21 +124,20 @@ function displayWinner() {
     }
 }
 
-const rockBtn = document.querySelector('#rock');
-rockBtn.addEventListener('click', playRound('rock', ));
+/*const rockBtn = document.querySelector('.rock');
+rockBtn.addEventListener('click', () {
+	playRound('rock', );
+});
 
-const paperBtn = document.querySelector('#paper');
-paperBtn.addEventListener('click', playRound('paper', ));
+const paperBtn = document.querySelector('.paper');
+paperBtn.addEventListener('click', () {
+	playRound('paper', );
+});
 
-const scissorsBtn = document.querySelector('#scissors');
-scissorsBtn.addEventListener('click', playRound('scissors', ));
+const scissorsBtn = document.querySelector('.scissors');
+scissorsBtn.addEventListener('click', () {
+	playRound('scissors', );
+});*/
 
 
-
-
-
-console.log(game());
-console.log('Final Score!')
-console.log('Player: ' + playerScore);
-console.log('Computer: ' + computerScore);
-console.log(displayWinner());
+wantToPlay();
