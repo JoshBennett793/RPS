70045beae0c6d3a45337aaa2395564da1cf70319
Josh Bennett <josh.bennett77@yahoo.com>
2022-04-13 13:17:01 -0600

    Add function to populate gameplay window
    Added a function that would remove the rules-popup and populate the
    gameplay window. Included is a round counter, computer and player score
    displays, and weapons choices. A function was written to remove the
    unselected weapons choices, leaving the selected weapons choice on the
    screen. Event listeners were attached to the weapons buttons to
    initiate a game round played with the corresponding weapon chosen.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function createDivElement (className, text, id = '') {
+function createDivElement(className, text, id = '') {
 	const div = document.createElement('div');
 	div.appendChild(document.createTextNode(text));
 	div.className = className;
@@ -10,8 +10,8 @@ function referToGithub() {
 	window.open('https://github.com/JoshBennett793/RPS')
 }
 
-function wantToPlay () {
-
+function wantToPlay() {
+	//build the UI for the game
 	const containerWantToPlay = createDivElement('start-popup', '')
 	containerWantToPlay.appendChild(createDivElement('', 'Want to play Rock, Paper, Scissors?'));
 
@@ -31,13 +31,13 @@ function wantToPlay () {
 	btnRed.addEventListener('click', () => {
 		return referToGithub();
 	});
-	
 }
 
 function showRules() {
 
 	const containerShowRules = createDivElement('rules-popup', '');
-	containerShowRules.appendChild(createDivElement('', 'Select Rock, Paper, or Scissors. Best 2 out of 3 rounds wins. '));
+	containerShowRules.appendChild(createDivElement('', 'Select Rock, Paper, or Scissors.'));
+	containerShowRules.appendChild(createDivElement('', 'Best 2 out of 3 rounds wins the game.'));
 
 	const buttonContainer = createDivElement('container-buttons', '');
 	buttonContainer.appendChild(createDivElement('button btn-green', 'Play'));
@@ -48,11 +48,82 @@ function showRules() {
 	const btnPlay = document.querySelector('.btn-green');
 	btnPlay.addEventListener('click', () => {
 		document.body.removeChild(containerShowRules);
+		startGame();
 	});
 }
 
+function startGame() {
+	
+	const containerGame = createDivElement('game-container', '');
+	
+	const containerRoundDisplay = createDivElement('round-display', '');
+	containerRoundDisplay.appendChild(createDivElement('', 'Round: 1'));
+
+
+	const containerComputerScore = createDivElement('computer', 'Computer');
+	containerComputerScore.appendChild(createDivElement('comp-score', '0'));
+
+	const containerPlayerScore = createDivElement('player', 'Player');
+	containerPlayerScore.appendChild(createDivElement('play-score', '0'));
+	
+	const containerWeapons = createDivElement('weapons-buttons', '');
+	containerWeapons.appendChild(createDivElement('button btn-rock', 'Rock'));
+	containerWeapons.appendChild(createDivElement('button btn-paper' , 'Paper'));
+	containerWeapons.appendChild(createDivElement('button btn-scissors', 'Scissors'));
+
+	const messageDisplay = createDivElement('message-bar', '')
+	messageDisplay.appendChild(createDivElement('message', ));
+
+	containerGame.appendChild(containerRoundDisplay);
+	containerGame.appendChild(containerComputerScore);
+	containerGame.appendChild(containerPlayerScore);
+	containerGame.appendChild(containerWeapons);
+	document.body.appendChild(containerGame);
+
+	const rockBtn = document.querySelector('.btn-rock');
+	rockBtn.addEventListener('click', () => {
+		containerGame.removeChild(containerWeapons);
+		containerGame.appendChild(createDivElement('chosen-weapon', 'Rock'));
+		playRound('rock', );
+	});
+
+	const paperBtn = document.querySelector('.btn-paper');
+	paperBtn.addEventListener('click', () => {
+		containerGame.removeChild(containerWeapons);
+		containerGame.appendChild(createDivElement('chosen-weapon', 'Paper'));
+		playRound('paper', );
+	});
+
+	const scissorsBtn = document.querySelector('.btn-scissors');
+	scissorsBtn.addEventListener('click', () => {
+		containerGame.removeChild(containerWeapons);
+		containerGame.appendChild(createDivElement('chosen-weapon', 'Scissors'));
+		playRound('scissors', );
+	});
+} // startGame()
+
+function updateComputerScore(computerScore) {
+	
+	document.getElementsByClassName('.comp-score').replaceWith(createDivElement('comp-score', `${computerScore}`));
+}
+
+function updatePlayerScore(playerScore) {
+	document.getElementsByClassName('.play-score').replaceWith(createDivElement('play-score', `${playerScore}`));
+}
+
+// resets weapon selection buttons and starts a new round
+function startNewRound() {
+}
+
+//displays computer's choice in similar button design
+function displayComputerChoice(computerSelection) {
+	document.containerGame.createDivElement('computer-choice-display', `${computerSelection}`);
+}
+
+//GAME LOGIC
+
 // Computer random choice function
-function computerPlay () {
+function computerPlay() {
   const choices = ['rock', 'paper', 'scissors'];
   let computerSelection = choices[Math.floor(Math.random() * choices.length)];
   return computerSelection;
@@ -89,7 +160,6 @@ function playRound(playerSelection, computerSelection) {
 
 // 5 round game function
 /*function game() {
-//for (let i = 0; i < 5; i++) {
   	computerSelection = computerPlay();
 	  let result = playRound(playerSelection, computerSelection);
   	  if (result == 'win') {
@@ -123,21 +193,6 @@ function displayWinner() {
     return 'It\'s a tie!'
     }
 }
-
-/*const rockBtn = document.querySelector('.rock');
-rockBtn.addEventListener('click', () {
-	playRound('rock', );
-});
-
-const paperBtn = document.querySelector('.paper');
-paperBtn.addEventListener('click', () {
-	playRound('paper', );
-});
-
-const scissorsBtn = document.querySelector('.scissors');
-scissorsBtn.addEventListener('click', () {
-	playRound('scissors', );
-});*/
 
 
 wantToPlay();
